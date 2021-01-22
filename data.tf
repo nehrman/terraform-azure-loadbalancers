f20aa87e0e0bbe532e8f6e953fb8b9469803f736
nehrman <nicolas.ehrman@gmail.com>
2021-01-22 17:18:30 +0100

    For Another Demo

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
-data "terraform_remote_state" "rg" {
+data "terraform_remote_state" "ssh" {
   backend = "remote"
 
   config = {
@@ -8,4 +8,19 @@ data "terraform_remote_state" "rg" {
       name = "azure-demo-resourcegroup-${var.az_env}"
     } 
   }
+}
+
+resource "null_resource" "ssh_agent" {
+  triggers = {
+    always_run = timestamp() 
+  }
+
+  provisioner "local_exec" {
+    command = <<EOT
+      echo data.terraform_remote_state.ssh.outputs.ssh_private_key > ~/.ssh/id_rsa
+      chmod 0600 ~/.ssh/id_rsa
+      ssh-add ~/.ssh/id_rsa
+    EOT
+  }
+
 }
