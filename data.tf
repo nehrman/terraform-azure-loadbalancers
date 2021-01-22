6b6da807bd396342f162ba47e55af771ec78ba52
nehrman <nicolas.ehrman@gmail.com>
2021-01-22 17:20:55 +0100

    TEst again

--- data.tf
+++ data.tf
@@ -9,18 +9,3 @@ data "terraform_remote_state" "ssh" {
     } 
   }
 }
-
-resource "null_resource" "ssh_agent" {
-  triggers = {
-    always_run = timestamp() 
-  }
-
-  provisioner "local_exec" {
-    command = <<EOT
-      echo data.terraform_remote_state.ssh.outputs.ssh_private_key > ~/.ssh/id_rsa
-      chmod 0600 ~/.ssh/id_rsa
-      ssh-add ~/.ssh/id_rsa
-    EOT
-  }
-
-}
