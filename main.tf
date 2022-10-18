3dce849934303366a291a58dfdcab00673f894ca
Nicolas EHRMAN <nicolas.ehrman@gmail.com>
2022-10-18 10:46:24 +0200

    .

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@
 
 module "lb" {
   source                   = "app.terraform.io/Nehrman-Demo/lb/azurerm"
-  version                  = "3.0.0"
+  version                  = "3.0.1"
   tf_az_name               = var.az_name
   tf_az_env                = var.az_env
   tf_az_location           = var.az_location
